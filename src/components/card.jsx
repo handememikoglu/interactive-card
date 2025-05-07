e45ee6feee25fd36438536cab09d55fd29fa283e
Hande Eylül Memikoğlu <handememikoglu@gmail.com>
2025-05-07 13:56:51 +0300

    cvv kodu için kart arka planı ayarlandı.

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,24 +2,40 @@ import Image from "next/image";
 
 export default function Card({ cardName, cardNumber, expMonth, expYear, cvc }) {
   return (
-    <div className="relative w-[350px] h-[200px]">
-      <Image
-        src="/bg-card-front.png"
-        alt="Card Front"
-        width={350}
-        height={200}
-        className="rounded-xl shadow-lg"
-      />
-      <div className="absolute top-6 left-6 text-white w-[300px]">
-        <p className="text-lg tracking-widest mb-6">
-          {cardNumber || "0000 0000 0000 0000"}
+    <div className="relative w-[400px] h-[250px]">
+      {/* Back Card */}
+      <div className="absolute top-0 right-0 z-0">
+        <Image
+          src="/bg-card-back.png"
+          alt="Card Back"
+          width={350}
+          height={200}
+          className="rounded-xl shadow-lg"
+        />
+        <p className="absolute top-[85px] right-10 text-white text-sm tracking-widest">
+          {cvc || "000"}
         </p>
-        <div className="flex justify-between text-sm uppercase">
-          <p>{cardName || "Jane Appleseed"}</p>
-          <p>{(expMonth || "00") + "/" + (expYear || "00")}</p>
-        </div>
       </div>
 
+      {/* Front Card */}
+      <div className="absolute top-30 left-0 z-10">
+        <Image
+          src="/bg-card-front.png"
+          alt="Card Front"
+          width={350}
+          height={200}
+          className="rounded-xl shadow-lg"
+        />
+        <div className="absolute top-6 left-6 text-white w-[300px]">
+          <p className="text-lg tracking-widest mb-6">
+            {cardNumber || "0000 0000 0000 0000"}
+          </p>
+          <div className="flex justify-between text-sm uppercase">
+            <p>{cardName || "Jane Appleseed"}</p>
+            <p>{(expMonth || "00") + "/" + (expYear || "00")}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
